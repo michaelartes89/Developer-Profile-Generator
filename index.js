6d704ef462df8ef1a79fa874d48e9d4bcdad6c42
MDBA <artes.mike@gmail.com>
2019-12-13 19:34:00 -0500

    adding generate html

--- index.js
+++ index.js
@@ -22,4 +22,25 @@ inquirer
         console.log(JSON.stringify(response))
         console.log(response.username);
         console.log(response.color);
+        console.log(generateHTML(response));
 });
+
+function generateHTML(answers){
+    return `<!DOCTYPE html>
+    <html lang='en'>
+    <head>
+        <meta charset='UTF-8'>
+        <meta name='viewport' content="width=device-width, initial-scale=1">
+        <link href="assets/css/style.css" rel="stylesheet">
+        <style>
+            body {
+                background-color: ${answers.color}
+            }
+        </style>
+        <title>${answers.username} Resume Profile Profile</title>
+    </head>
+    <body>
+        <h1>${answers.username}</h1>
+    </body>
+    </html>`
+}
